1bded9fdb187b7d963daa855966ed8c21363c3b0
wengelawitgirma799 <141363872+wengelawitgirma799@users.noreply.github.com>
2024-09-11 20:25:02 +0300

    firstc commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div>
+        hello yeab
+      </div>
+       
+    
+  )
+}
+
+export default App
